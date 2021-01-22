abdd417e1fedf6aa23b283edb4e521ad58d657b1
Korver Huang <korver2017@gmail.com>
2021-01-22 16:04:13 +0800

    Feat: Add style to employee cards

--- src/components/employees/Employees.js
+++ src/components/employees/Employees.js
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react';
 import './Employees.css';
 
 import $api from 'axios';
+import rq from 'random-quotes';
+
+import { Card } from 'react-bootstrap';
 
 function Employees () {
 
@@ -14,14 +17,32 @@ function Employees () {
         console.log (res.data.results);
 
         const raw = [...res.data.results];
+        const variant = ['Primary', 'Secondary', 'Success', 'Danger', 'Warning', 'Info', 'Light', 'Dark'];
 
-        const list = raw.map (employee => (
-          <li>{ employee.name.first }</li>
+        const list = raw.map ((employee, i) => (
+
+          <Card
+            bg={variant[i % variant.length].toLowerCase()}
+            key={i}
+            text={variant[i % variant.length].toLowerCase() === 'light' ? 'dark' : 'white'}
+            style={{ width: '18rem', marginLeft: 100 + i * Math.random () * 10 }}
+            className="mb-2"
+          >
+            <Card.Header>{employee.name.first} {employee.name.last}</Card.Header>
+            <Card.Body>
+              <Card.Title>{variant[i % variant.length]} Card Title </Card.Title>
+              <Card.Text>
+                My Declaration to Win The Prize:
+              </Card.Text>
+              <Card.Text>
+                { rq ().body }
+              </Card.Text>
+            </Card.Body>
+          </Card>
         ))
 
         setEmployees (list);
-        
-      })
+      });
     
   }, []);
   
