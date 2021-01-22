9bd2c51d70a1f34dd581f0c8bf9eda0f22790606
Korver Huang <korver2017@gmail.com>
2021-01-22 12:12:50 +0800

    Chore: Initialize project

---
+++ src/components/employees/Employees.js
@@ -0,0 +1,15 @@
+import logo from '../../logo.svg';
+import './Employees.css';
+
+function Employees () {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Employees</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Employees;
