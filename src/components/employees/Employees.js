926cc7a984e702313ced1405f909de7e230cc8ac
Korver Huang <korver2017@gmail.com>
2021-01-29 17:02:47 +0800

    Feat: Modify coding style & add useEffect dependencies

--- src/components/employees/Employees.js
+++ src/components/employees/Employees.js
@@ -12,24 +12,21 @@ import { v4 as uuidv4 } from 'uuid';
 // CSS Framework
 import { Card } from 'react-bootstrap';
 
-function Employees (props) {
+function Employees(props) {
 
-  const [employees, setEmployees] = useState ([]);
-  const [triggerModalCount, setTriggerModalCount] = useState (0);
-  const [modalType, setModalType] = useState (null);
-  const [editTarget, setEditTarget] = useState ({});
-  // const [deleteTarget, setDeleteTarget] = useState ({});
+  const [employees, setEmployees] = useState([]);
+  const [triggerModalCount, setTriggerModalCount] = useState(0);
+  const [modalType, setModalType] = useState(null);
+  const [editTarget, setEditTarget] = useState({});
 
   // Show modal & pass selected employee data to modal.
   const handleEditModalData = (dataset, i) => {
 
     setEditTarget ({...dataset.employee, order: i});
 
-    console.log (dataset.type);
-
     // Trigger direct type of children show or hide modal by update trigger count.
-    setTriggerModalCount (triggerModalCount + 1);
-    setModalType (dataset.type);
+    setTriggerModalCount(triggerModalCount + 1);
+    setModalType(dataset.type);
   };
 
   // Update selected employee's data.
@@ -37,7 +34,7 @@ function Employees (props) {
 
     const pos = editTarget.order;
 
-    setEmployees (prevEmployees => {
+    setEmployees(prevEmployees => {
 
       prevEmployees[pos].name = editedEmployee.name;
       prevEmployees[pos].quote = editedEmployee.quote;
@@ -52,68 +49,64 @@ function Employees (props) {
 
     const pos = editTarget.order;
 
-    setEmployees (prevEmployees => {
+    setEmployees(prevEmployees => {
 
       prevEmployees.splice (pos, 1);
-
-      console.log (prevEmployees);
 
       const updatedEmployees = [...prevEmployees];
       return updatedEmployees;
     });
   };
 
-  useEffect (() => {
+  useEffect(() => {
 
     // Retrieve (initialize) employees' data.
-    $api.get ('https://randomuser.me/api/?results=10')
+    $api.get('https://randomuser.me/api/?results=10')
       .then (res => {
         
         const cleanSource = [...res.data.results];
-        const employeeList = cleanSource.map (employee => {
-          return { name: employee.name, quote: rq ().body, id: uuidv4 () };
+        const employeeList = cleanSource.map(employee => {
+          return {name: employee.name, quote: rq().body, id: uuidv4()};
         });
         
-        setEmployees (employeeList);
+        setEmployees(employeeList);
       });
   }, []);
 
-  useEffect (() => {
+  // useEffect(() => {
 
-    console.log (props);
+  //   if (! props.addedEmployee)
+  //     return;
 
-    if (! props.addedEmployee)
-      return;
+  //   setEmployees ([...employees, props.addedEmployee]);
 
-    setEmployees ([...employees, props.addedEmployee]);
-
-  }, [props.addedEmployee]);
+  // }, [props.addedEmployee]);
   
   return (
     <div className="employees">
       <div className="container">
         <div className="row">
           {
-            employees.map ((employee, i) => (
+            employees.map((employee, i) => (
               <Card
               bg="light"
-              key={ employee.id }
+              key={employee.id}
               text="dark"
               className='mb-2 position-relative'
               >
-                <Card.Header className="font-weight-bold">{ employee.name.first } { employee.name.last }</Card.Header>
+                <Card.Header className="font-weight-bold">{employee.name.first} { employee.name.last}</Card.Header>
                 <Card.Body>
                   <Card.Text className="font-weight-bold">
                     My Declaration to Win The Prize:
                   </Card.Text>
                   <Card.Text className="text-left">
-                    { employee.quote }
+                    {employee.quote}
                   </Card.Text>
                 </Card.Body>
 
                 <div className="editor position-absolute">
-                  <i onClick={ () => handleEditModalData ( { employee, type: 'edit' }, i) } className="fas fa-edit edit"></i>
-                  <i onClick={ () => handleEditModalData ( { employee, type: 'delete' }, i) }  className="fas fa-trash trash"></i>
+                  <i onClick={() => handleEditModalData({employee, type: 'edit'}, i)} className="fas fa-edit edit"></i>
+                  <i onClick={() => handleEditModalData({employee, type: 'delete'}, i)} className="fas fa-trash trash"></i>
                 </div>
               </Card>
             ))
@@ -121,8 +114,8 @@ function Employees (props) {
         </div>
       </div>
 
-      <EmployeeEditModal triggerModalCount={ triggerModalCount } modalType={ modalType } editTarget={ editTarget } handleEditEmployee={ handleEditEmployee }></EmployeeEditModal>
-      <EmployeeDeleteModal triggerModalCount={ triggerModalCount } modalType={ modalType } editTarget={ editTarget } handleDeleteEmployee={ handleDeleteEmployee }></EmployeeDeleteModal>
+      <EmployeeEditModal triggerModalCount={triggerModalCount} modalType={modalType} editTarget={editTarget} handleEditEmployee={handleEditEmployee}></EmployeeEditModal>
+      <EmployeeDeleteModal triggerModalCount={triggerModalCount} modalType={modalType} editTarget={editTarget} handleDeleteEmployee={handleDeleteEmployee}></EmployeeDeleteModal>
     </div>
   );
 }
--- src/components/modal/EmployeeEditModal/EmployeeEditModal.js
+++ src/components/modal/EmployeeEditModal/EmployeeEditModal.js
@@ -2,76 +2,73 @@
 import { useState, useEffect } from 'react';
 import './EmployeeEditModal.css';
 
-// JavaScript Plugin
-import { v4 as uuidv4 } from 'uuid';
-
 // CSS Framework
 import { Button, Modal, Form } from 'react-bootstrap';
 
 function EmployeeEditModal ({triggerModalCount, modalType, editTarget, handleEditEmployee}) {
 
-  const [show, setShow] = useState (false);
+  const [show, setShow] = useState(false);
 
   // Initialize employee's data to be edited.
-  const [input, setInput] = useState ({ name: { first: '', last: '' }, quote: '', id: '' });
-  const handleCloseLightbox = () => setShow (false);
+  const [input, setInput] = useState({name: {first: '', last: ''}, quote: ''});
+  const handleCloseLightbox = () => setShow(false);
 
   // Edit employee data
   const handleEdit = () => {
 
-    console.log ('props.handleEdit');
+    const first = document.querySelector('.first').value;
+    const last = document.querySelector('.last').value;
+    const quote = document.querySelector('.quote').value;
 
-    const first = document.querySelector ('.first').value;
-    const last = document.querySelector ('.last').value;
-    const quote = document.querySelector ('.quote').value;
-    const id = uuidv4 ();
+    if (!first || !last || !quote)
+      return alert('Sorry, columns may not be empty.');
     
-    handleEditEmployee ({ name: { first: first, last: last }, quote: quote, id: id });
+    handleEditEmployee({name: {first, last}, quote});
 
     // Close Lightbox
-    handleCloseLightbox ();
+    handleCloseLightbox();
   }
 
-  useEffect (() => {
+  useEffect(() => {
 
     // If parent component just initialize, return.
     if (triggerModalCount <= 0 || modalType !== 'edit')
       return;
 
     // Set up employee props data, then show lightbox.
-    setInput ({ name: { first: editTarget.name.first, last:  editTarget.name.last }, quote: editTarget.quote });
+    setInput({name: {first: editTarget.name.first, last: editTarget.name.last}, quote: editTarget.quote});
     setShow (true);
     
-  }, [triggerModalCount]);
+  }, [triggerModalCount, modalType, editTarget]);
 
   return (
     <>
-      <Modal show={ show } onHide={ handleCloseLightbox }>
+      <Modal show={show} onHide={handleCloseLightbox}>
         
         <Modal.Header closeButton>
-          <Modal.Title>Edit Employee - { input.name.first } { input.name.last }</Modal.Title>
+          <Modal.Title>Edit Employee - {input.name.first} {input.name.last}</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
 
           <Form.Row className="mt-2 mb-4">
 
-            <Form.Control className="first col-5 mr-5" defaultValue={ input.name.first } type="text" placeholder="First Name" />
-            <Form.Control className="last col-5" defaultValue={ input.name.last } type="text" placeholder="First Name" />
+            <Form.Control className="first col-5 mr-5" defaultValue={input.name.first} type="text" placeholder="First Name" />
+            <Form.Control className="last col-5" defaultValue={input.name.last} type="text" placeholder="First Name" />
 
           </Form.Row>
 
           <Form.Row>
-            <Form.Control className="quote" defaultValue={ input.quote } placeholder="My Declaration to Win The Prize!" as="textarea" rows={3} />
+            <Form.Control className="quote" defaultValue={input.quote} placeholder="My Declaration to Win The Prize!" as="textarea" rows={3} />
           </Form.Row>
           
         </Modal.Body>
         <Modal.Footer>
 
-          <Button variant="warning" onClick={ handleCloseLightbox }>
+          <Button variant="warning" onClick={handleCloseLightbox}>
             Cancel
           </Button>
-          <Button variant="success" onClick={ handleEdit }>
+          <Button variant="success" onClick={handleEdit}>
             Update
           </Button>
           
