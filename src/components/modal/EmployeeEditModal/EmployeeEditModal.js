c04595349a1ada9c61cf8173dc627c1bbf19bae2
Korver <korver2017@gmail.com>
2021-03-14 00:38:02 +0800

    Feat: Replace EmployeeEditModal component

--- src/components/modal/EmployeeEditModal/EmployeeEditModal.js
+++ src/components/modal/EmployeeEditModal/EmployeeEditModal.js
@@ -1,81 +1,115 @@
 // React & Component
 import { useState, useEffect } from 'react';
-import './EmployeeEditModal.css';
+// import './EmployeeEditModal.css';
 
-// CSS Framework
-import { Button, Modal, Form } from 'react-bootstrap';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
 
 function EmployeeEditModal ({triggerModalCount, modalType, editTarget, handleEditEmployee}) {
 
-  const [show, setShow] = useState(false);
+  const [open, setOpen] = useState (false);
+  const theme = useTheme ();
+  const fullScreen = useMediaQuery (theme.breakpoints.down ('sm'));
 
   // Initialize employee's data to be edited.
-  const [input, setInput] = useState({name: {first: '', last: ''}, quote: ''});
-  const handleCloseModal = () => setShow(false);
+  // const [input, setInput] = useState({name: {first: '', last: ''}, quote: ''});
+  const handleCloseModal = () => setOpen (false);
 
-  // Edit employee data
-  const handleEdit = () => {
-
-    const first = document.querySelector('.first').value;
-    const last = document.querySelector('.last').value;
-    const quote = document.querySelector('.quote').value;
-
-    if (!first || !last || !quote)
-      return alert('Sorry, columns may not be empty.');
-    
-    handleEditEmployee({name: {first, last}, quote});
-
-    // Close Modal
-    handleCloseModal();
-  }
-
-  useEffect(() => {
+  useEffect (() => {
 
     // If parent component just initialize, return.
     if (triggerModalCount <= 0 || modalType !== 'edit')
       return;
 
     // Set up employee props data, then show lightbox.
-    setInput({name: {first: editTarget.name.first, last: editTarget.name.last}, quote: editTarget.quote});
-    setShow (true);
+    // setInput({name: {first: editTarget.name.first, last: editTarget.name.last}, quote: editTarget.quote});
+    setOpen (true);
     
   }, [triggerModalCount, modalType, editTarget]);
 
   return (
-    <>
-      <Modal show={show} onHide={handleCloseModal}>
-        
-        <Modal.Header closeButton>
-          <Modal.Title>Edit Employee - {input.name.first} {input.name.last}</Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>
-
-          <Form.Row className="mt-2 mb-4">
-
-            <Form.Control className="first col-5 mr-5" defaultValue={input.name.first} type="text" placeholder="First Name" />
-            <Form.Control className="last col-5" defaultValue={input.name.last} type="text" placeholder="Last Name" />
-
-          </Form.Row>
-
-          <Form.Row>
-            <Form.Control className="quote" defaultValue={input.quote} placeholder="My Declaration to Win The Prize!" as="textarea" rows={3} />
-          </Form.Row>
-          
-        </Modal.Body>
-        <Modal.Footer>
-
-          <Button variant="warning" onClick={handleCloseModal}>
-            Cancel
-          </Button>
-          <Button variant="success" onClick={handleEdit}>
-            Update
-          </Button>
-          
-        </Modal.Footer>
-      </Modal>
-    </>
+    <Dialog
+      fullScreen={fullScreen}
+      open={open}
+      onClose={handleCloseModal}
+      aria-labelledby="responsive-dialog-title"
+    >
+      <DialogTitle id="responsive-dialog-title">{"Use Google's location service?"}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Let Google help apps determine location. This means sending anonymous location data to
+          Google, even when no apps are running.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button autoFocus onClick={handleCloseModal} color="primary">
+          Disagree
+        </Button>
+        <Button onClick={handleCloseModal} color="primary" autoFocus>
+          Agree
+        </Button>
+      </DialogActions>
+    </Dialog>
   )
+
+  // Edit employee data
+  // const handleEdit = () => {
+
+  //   const first = document.querySelector('.first').value;
+  //   const last = document.querySelector('.last').value;
+  //   const quote = document.querySelector('.quote').value;
+
+  //   if (!first || !last || !quote)
+  //     return alert('Sorry, columns may not be empty.');
+    
+  //   handleEditEmployee({name: {first, last}, quote});
+
+  //   // Close Modal
+  //   handleCloseModal();
+  // }
+
+  // return (
+
+  //   <>
+  //     <Modal show={show} onHide={handleCloseModal}>
+        
+  //       <Modal.Header closeButton>
+  //         <Modal.Title>Edit Employee - {input.name.first} {input.name.last}</Modal.Title>
+  //       </Modal.Header>
+
+  //       <Modal.Body>
+
+  //         <Form.Row className="mt-2 mb-4">
+
+  //           <Form.Control className="first col-5 mr-5" defaultValue={input.name.first} type="text" placeholder="First Name" />
+  //           <Form.Control className="last col-5" defaultValue={input.name.last} type="text" placeholder="Last Name" />
+
+  //         </Form.Row>
+
+  //         <Form.Row>
+  //           <Form.Control className="quote" defaultValue={input.quote} placeholder="My Declaration to Win The Prize!" as="textarea" rows={3} />
+  //         </Form.Row>
+          
+  //       </Modal.Body>
+  //       <Modal.Footer>
+
+  //         <Button variant="warning" onClick={handleCloseModal}>
+  //           Cancel
+  //         </Button>
+  //         <Button variant="success" onClick={handleEdit}>
+  //           Update
+  //         </Button>
+          
+  //       </Modal.Footer>
+  //     </Modal>
+  //   </>
+  // )
 }
 
 export default EmployeeEditModal;
