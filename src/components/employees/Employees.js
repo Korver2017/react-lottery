ec130df611d0c3c679cec489569e851cd2075588
Korver <korver2017@gmail.com>
2021-03-22 12:06:36 +0800

    Chore: Add comments to Employees component

--- src/components/employees/Employees.js
+++ src/components/employees/Employees.js
@@ -1,18 +1,20 @@
-// React & Component
-import { useState, useEffect } from 'react';
-import './Employees.css';
-import AddEmployeeModal from '../modal/AddEmployeeModal';
-import EditEmployeeModal from '../modal/EditEmployeeModal';
-import DeleteEmployeeModal from '../modal/DeleteEmployeeModal';
-
-// JavaScript Plugin
+/**
+ *
+ * JavaScript Plugins
+ *
+ */
 import $api from 'axios';
 import rq from 'random-quotes';
 import { v4 as uuidv4 } from 'uuid';
 
-// CSS Framework
-// import { Card } from 'react-bootstrap';
+
+/**
+ *
+ * React & Material UI
+ *
+ */
 import { makeStyles } from '@material-ui/core/styles';
+import { useState, useEffect } from 'react';
 import Divider from '@material-ui/core/Divider';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
@@ -23,6 +25,22 @@ import IconButton from '@material-ui/core/IconButton';
 import CreateRoundedIcon from '@material-ui/icons/CreateRounded';
 import DeleteForeverRoundedIcon from '@material-ui/icons/DeleteForeverRounded';
 
+
+/**
+ *
+ * Components
+ *
+ */
+import AddEmployeeModal from '../modal/AddEmployeeModal';
+import EditEmployeeModal from '../modal/EditEmployeeModal';
+import DeleteEmployeeModal from '../modal/DeleteEmployeeModal';
+
+
+/**
+ *
+ * Styles Settings
+ *
+ */
 const useStyles = makeStyles (theme => ({
   root: {
     padding: theme.spacing (2),
@@ -45,36 +63,78 @@ const useStyles = makeStyles (theme => ({
   }
 }));
 
+
+/**
+ *
+ * Employees
+ *
+ */
 const Employees = () => {
 
+  // Apply styles.
+  const classes = useStyles ();
+
+  // State of employees.
+  const [employees, setEmployees] = useState ([]);
+
+  // State of open AddEmployeeModal.
   const [openAddEmployeeModal, setOpenAddEmployeeModal] = useState (false);
-  const [employees, setEmployees] = useState([]);
-  const [addedEmployee, setAddedEmployee] = useState({});
-  const [editEmployee, setEditEmployee] = useState({});
-  const [deleteEmployee, setDeleteEmployee] = useState({});
 
-  // Show modal & pass selected employee data to modal.
+  // State of employee will be updated.
+  const [addEmployee, setAddEmployee] = useState ({});
+  const [editEmployee, setEditEmployee] = useState ({});
+  const [deleteEmployee, setDeleteEmployee] = useState ({});
+
+
+  /**
+   *
+   * Open & Pass Employee Data to Edit Employee Modal
+   *
+   */
   const handleEditModalData = (employee, i) => {
-
-    setEditEmployee ({...employee, order: i});
-  };
-
-  const handleDeleteModalData = (employee, i) => {
-    setDeleteEmployee ({...employee, order: i});
+    return setEditEmployee ({...employee, order: i});
   }
 
+
+  /**
+   *
+   * Open & Pass Employee Data to Delete Employee Modal
+   *
+   */
+  const handleDeleteModalData = (employee, i) => {
+    return setDeleteEmployee ({...employee, order: i});
+  }
+
+
+  /**
+   *
+   * Open / Hide Add Employee Modal
+   *
+   */
   const handleToggleModal = () => {
     return setOpenAddEmployeeModal (prevState => ! prevState);
   }
   
-  const handleAddEmployee = (addedEmployee) => setAddedEmployee (addedEmployee);
 
-  // Update selected employee's data.
+  /**
+   *
+   * Handle Add New Employee
+   *
+   */
+  const handleAddEmployee = (addEmployee) => setAddEmployee (addEmployee);
+
+
+  /**
+   *
+   * Update Selected Employee's Data
+   *
+   */
   const handleEditEmployee = (editedEmployee) => {
 
     const pos = editEmployee.order;
 
-    setEmployees(prevEmployees => {
+    // Update employee data.
+    setEmployees (prevEmployees => {
 
       prevEmployees[pos].name = editedEmployee.name;
       prevEmployees[pos].quote = editedEmployee.quote;
@@ -84,12 +144,18 @@ const Employees = () => {
     });
   };
 
-  // Delete selected employee's data.
+
+  /**
+   *
+   * Delete Selected Employee's Data
+   *
+   */
   const handleDeleteEmployee = () => {
 
     const pos = editEmployee.order;
 
-    setEmployees(prevEmployees => {
+    // Update employee data.
+    setEmployees (prevEmployees => {
 
       prevEmployees.splice (pos, 1);
 
@@ -98,41 +164,62 @@ const Employees = () => {
     });
   };
 
+
+  /**
+   *
+   * Initialize Employees
+   *
+   */
   useEffect(() => {
 
-    // Retrieve (initialize) employees' data.
-    $api.get('https://randomuser.me/api/?results=10')
+    // Retrieve employees' data.
+    $api.get ('https://randomuser.me/api/?results=10')
       .then (res => {
         
+        // Parsing data.
         const cleanSource = [...res.data.results];
-        const employeeList = cleanSource.map(employee => {
-          return {name: employee.name, quote: rq().body, id: uuidv4()};
+        const employeeList = cleanSource.map (employee => {
+          return {name: employee.name, quote: rq ().body, id: uuidv4 ()};
         });
         
-        setEmployees(employeeList);
+        setEmployees (employeeList);
       });
   }, []);
 
-  useEffect(() => {
 
-    if (Object.keys(addedEmployee).length <= 0) 
+  /**
+   *
+   * Add New Employee
+   *
+   */
+  useEffect (() => {
+
+    // Return when component just initialized.
+    if (Object.keys (addEmployee).length <= 0) 
       return;
 
-    setEmployees ([...employees, addedEmployee]);
+    setEmployees ([...employees, addEmployee]);
 
-  }, [addedEmployee]);
-
-  const classes = useStyles ();
+  }, [addEmployee]);
   
+
+  /**
+   *
+   * JSX
+   *
+   */
   return (
     <div className="employees">
       <Grid container>
         {
           employees.map ((employee, i) => (
             <Grid key={employee.id} className={classes.root} item xs={3}>
+
               <Card className={classes.card}>
+
                 <CardHeader title={employee.name.first + ' ' + employee.name.last} />
                 <Divider variant="middle" />
+
                 <CardContent>
                   <p className={classes.sub}>My Declaration to <br /> Win The Prize:</p>
                   {employee.quote}
@@ -148,6 +235,7 @@ const Employees = () => {
                   </IconButton>
                 </Box>
               </Card>
+              
             </Grid>
           ))
         }
