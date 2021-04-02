d0d21b9999cb321248fb5f9de10f2d9a9f73c6f5
Korver <korver2017@gmail.com>
2021-04-02 18:50:00 +0800

    feat: Initialize const data

---
+++ src/const.js
@@ -0,0 +1,25 @@
+const employees = {
+  first: '',
+  last: '',
+  declaration: '',
+  department: '',
+};
+
+const prizes = [{
+  name: 'Macbook Pro',
+  level: 'premium',
+  count: '3',
+  intro: 'QWER',
+}, {
+  name: 'Macbook Air',
+  level: 'high',
+  count: '5',
+  intro: 'QWER',
+}, {
+  name: 'PS4',
+  level: 'medium',
+  count: '1',
+  intro: 'QWER',
+}];
+
+export { employees, prizes };
