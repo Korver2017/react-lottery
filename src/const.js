e1379af18922a2dfda034e979f1ee14c69843532
Korver <korver2017@gmail.com>
2021-04-03 00:10:40 +0800

    chore: Remove useless code & add comments

--- src/const.js
+++ src/const.js
@@ -1,4 +1,7 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const employees = {
+  id: uuidv4 (),
   first: '',
   last: '',
   declaration: '',
@@ -6,16 +9,19 @@ const employees = {
 };
 
 const prizes = [{
+  id: uuidv4 (),
   name: 'Macbook Pro',
   level: 'premium',
   count: '3',
   intro: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio, reprehenderit.',
 }, {
+  id: uuidv4 (),
   name: 'Macbook Air',
   level: 'high',
   count: '5',
   intro:  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel beatae vitae veritatis, praesentium magnam quas.',
 }, {
+  id: uuidv4 (),
   name: 'PS4',
   level: 'medium',
   count: '1',
--- src/views/Prizes.js
+++ src/views/Prizes.js
@@ -1,75 +1,63 @@
-import { useState, useEffect } from 'react';
+/**
+ *
+ * Material UI
+ *
+ */
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 
-import reactLogo from '../images/logo512.png';
-import { prizes } from '../const';
+
+/**
+ *
+ * Component
+ *
+ */
 import Prize from '../components/Prize';
 
-const useStyles = makeStyles((theme) => ({
+
+/**
+ *
+ * Const
+ *
+ */
+import { prizes } from '../const';
+
+
+/**
+ *
+ * Material UI Style
+ *
+ */
+const useStyles = makeStyles (theme => ({
   root: {
     marginTop: theme.spacing (3),
     paddingBottom: theme.spacing (3),
   },
-  gutter: {
-    padding: theme.spacing (2),
-  },
-  card: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  avatar: {
-    backgroundColor: red[500],
-  },
 }));
 
+
+/**
+ *
+ * Prizes
+ *
+ */
 const Prizes = () => {
 
-  const classes = useStyles();
-  const [expanded, setExpanded] = useState (false);
+  // Apply styles.
+  const classes = useStyles ();
 
-  const handleExpandClick = () => {
-    setExpanded (!expanded);
-  };
-
-  useEffect (() => {
-    console.log ('prizes: ', prizes);
-  }, []);
-
+  
+  /**
+   *
+   * JSX
+   *
+   */
   return (
     <div className="prizes">
       <Grid container className={classes.root}>
         {
           prizes.map (prize => (
-            <Prize prize={prize} />
+            <Prize key={prize.id} prize={prize} />
           ))
         }
       </Grid>
