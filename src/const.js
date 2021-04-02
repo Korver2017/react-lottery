d098f3cea3704464e79570ecd3c57f6846f5b229
Korver <korver2017@gmail.com>
2021-04-03 00:25:31 +0800

    Revert "chore: Add more constant prize data"
    This reverts commit ce3f60e1b2a3c3b4a8731d4a967d147f5bc98394.

--- src/const.js
+++ src/const.js
@@ -26,42 +26,6 @@ const prizes = [{
   level: 'medium',
   count: '1',
   intro:  'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, officiis.',
-},{
-  id: uuidv4 (),
-  name: 'Macbook Pro',
-  level: 'premium',
-  count: '3',
-  intro: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio, reprehenderit.',
-}, {
-  id: uuidv4 (),
-  name: 'Macbook Air',
-  level: 'high',
-  count: '5',
-  intro:  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel beatae vitae veritatis, praesentium magnam quas.',
-}, {
-  id: uuidv4 (),
-  name: 'PS4',
-  level: 'medium',
-  count: '1',
-  intro:  'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, officiis.',
-},{
-  id: uuidv4 (),
-  name: 'Macbook Pro',
-  level: 'premium',
-  count: '3',
-  intro: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio, reprehenderit.',
-}, {
-  id: uuidv4 (),
-  name: 'Macbook Air',
-  level: 'high',
-  count: '5',
-  intro:  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel beatae vitae veritatis, praesentium magnam quas.',
-}, {
-  id: uuidv4 (),
-  name: 'PS4',
-  level: 'medium',
-  count: '1',
-  intro:  'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, officiis.',
-},];
+}];
 
 export { employees, prizes };
