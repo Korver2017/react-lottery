4ad89c87594fc0fecbb76262f37b4814d3701986
Korver <korver2017@gmail.com>
2021-04-04 02:01:22 +0800

    chore: Add comments & recover index.css

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
+/**
+ *
+ * Initial Settings
+ *
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+/**
+ *
+ * Material UI
+ *
+ */
+
+// Font.
 import 'fontsource-roboto';
 
+// Theme.
 import { unstable_createMuiStrictModeTheme, ThemeProvider } from '@material-ui/core/styles';
 
+// Apply theme.
 const theme = unstable_createMuiStrictModeTheme ();
 
-ReactDOM.render(
+ReactDOM.render (
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <App />
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById ('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals ();
